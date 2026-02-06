2f581617beca5de790a7bbfa2383c15a9506b12c
suwapat40 <suwapat.ping@gmail.com>
2026-02-06 14:17:31 +0700

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,30 @@
+
+let n1 = 100;
+let n2 = 2.5;
+let a = "A";
+let b = "B";
+let c5 = "C5";
+let h = "Hello";
+
+console.log("--- ข้อ 1 ---");
+console.log(n1);
+console.log(n2);
+console.log(a);
+console.log(b);
+console.log(c5);
+
+console.log("--- ข้อ 2 ---");
+console.log(n1 + " " + n2);
+console.log(a + " " + b);
+console.log(c5);
+
+console.log("--- ข้อ 3 ---");
+console.log(n1 + "" + n2 + "  " + a + b + "  " + c5);
+
+console.log("--- ข้อ 4 ---");
+console.log(n1 + h);
+console.log(n2 + h);
+console.log(h + a);
+
+console.log("--- ข้อ 5 ---");
+console.log(n1 + h + n2 + h + a);
